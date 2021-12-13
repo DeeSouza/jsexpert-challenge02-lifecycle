574e1ccf8c6d9e787fceef469f9ba29f4b314f57
Diego Souza <dee-souza@hotmail.com>
2021-12-13 11:22:04 -0300

    implementeds functions ObjectMethodService

--- src/service/ObjectMethodsService.js
+++ src/service/ObjectMethodsService.js
@@ -1,18 +1,67 @@
 class ObjectMethodsService {
   getEnhancedObject(rawObject) {
-    // TODO: return an enhanced object implementing `valueOf` or `toString` depending on the context
-    return null;
+    const copyRawObject = this.getPropertiesObject(rawObject);
+
+    const newRawObject = {
+      ...copyRawObject,
+      valueOf() {
+        return this.age;
+      },
+    };
+
+    return newRawObject;
   }
 
   getEnhancedObject2(rawObject) {
-    // TODO: return an enhanced object implementing `valueOf` or `toString` depending on the context
-    return null;
+    const copyRawObject = this.getPropertiesObject(rawObject);
+    const stringifyObject = this.formattedAndMergeValues(copyRawObject);
+
+    const newRawObject = {
+      ...copyRawObject,
+      toString() {
+        return `[${stringifyObject}]`;
+      },
+    };
+
+    return newRawObject;
   }
 
   getEnhancedObjectWithoutValueOfOrToString(rawObject) {
-    // TODO: return an enhanced object **WITHOUT** implementing `valueOf` nor `toString`.
-    // Note: Implementing `toString` and `valueOf` will NOT be allowed in this function.
-    return null;
+    const copyRawObject = this.getPropertiesObject(rawObject);
+    const stringifyObject = this.formattedAndMergeValues(copyRawObject);
+
+    const newRawObject = {
+      ...copyRawObject,
+      [Symbol.toPrimitive](type) {
+        const types = {
+          string: `[${stringifyObject}]`,
+          number: this.age,
+        };
+
+        return types[type] || types["string"];
+      },
+    };
+
+    return newRawObject;
+  }
+
+  formattedAndMergeValues(object) {
+    const keyValuesRawObject = Object.entries(object);
+    const item = [];
+
+    for (let [key, value] of keyValuesRawObject) {
+      const formattedValue = typeof value === "number" ? value : `"${value}"`;
+
+      item.push(`${key}=${formattedValue}`);
+    }
+
+    const mergeItems = item.join(",");
+
+    return mergeItems;
+  }
+
+  getPropertiesObject(rawObject) {
+    return rawObject.__proto__;
   }
 }
 
